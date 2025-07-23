9a928bf69187890152ea78437fb915ff93c649e8
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2025-07-24 00:16:32 +0530

    Add StudentReg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import TeacherReg from './pages/Teacher/TeacherReg';
+import StudentReg from './pages/Student/StudentReg';
 
 function App() {
-  
+
 
   return (
     <>
-      <TeacherReg />
+      <StudentReg />
     </>
   )
 }
